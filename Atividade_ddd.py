e62a06d2f645a5589a9d341dde185288fb42c101
IGOR PEREIRA <97792380+Igorpereirag@users.noreply.github.com>
2022-10-25 09:20:12 -0300

    Update Atividade_ddd.py

--- Atividade_ddd.py
+++ Atividade_ddd.py
@@ -3,23 +3,30 @@
 
 #Dicionario cujo os ddds são como chaves assoaciadas as capitais
 ddds = {
-  "61": "brasilia",
-  "71": "salvador",
-  "11": "são paulo",
-  "21": "rio de janeiro",
-  "32": "juiz de fora",
-  "19": "campinhas",
-  "27": "vitoria",
-  "31": "belo horizonte"
+    "61": "brasilia",
+    "71": "salvador",
+    "11": "são paulo",
+    "21": "rio de janeiro",
+    "32": "juiz de fora",
+    "19": "campinhas",
+    "27": "vitoria",
+    "31": "belo horizonte"
 }
 while True:
-  #recebe o ddd do usuario
+# recebe o ddd do usuario
   nun = input("digite seu ddd")
-  #converte o ddd de inteiro para string
+  # converte o ddd de inteiro para string
   nun = str(nun)
-  #vare  o dicionario
+  validate = False
+  # vare  o dicionario
   for ddd in ddds:
-    # para cada ddd no dicionario igual ao que o usuario digitou
-    if ddd == nun:
-      #printa  o estado associado a chave/ddd
-      print(ddds[ddd])
+  # para cada ddd no dicionario igual ao que o usaurio digitou
+      if ddd == nun:
+        # printa  o estado associado a chave/ddd
+          print(ddds[ddd])
+          validate = True
+          break
+   #faz o tratamento de erro
+  if validate == False:
+    print("DDD invalido, tente novamente")
+
