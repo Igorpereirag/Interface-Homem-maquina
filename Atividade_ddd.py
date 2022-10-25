40f2110de8d48f35c1032735a4f87ae5e99fab10
IGOR PEREIRA <97792380+Igorpereirag@users.noreply.github.com>
2022-10-24 23:02:39 -0300

    Create Atividade_ddd.py

---
+++ Atividade_ddd.py
@@ -0,0 +1,16 @@
+ddds = {
+  "61": "brasilia",
+  "71": "salvador",
+  "11": "são paulo",
+  "21": "rio de janeiro",
+  "32": "juiz de fora",
+  "19": "campinhas",
+  "27": "vitoria",
+  "31": "belo horizonte"
+}
+while True:
+  nun = input("digite seu ddd")
+  nun = str(nun)
+  for ddd in ddds:
+    if ddd == nun:
+      print(ddds[ddd])
